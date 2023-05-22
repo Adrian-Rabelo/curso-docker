15b2af648c07358d05c90b1a44be533cb7bfa0b2
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2023-05-22 18:24:19 -0400

    escalando containers

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -5,8 +5,9 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+    print('Aguardando mensagens...')
     while True:
-        mensagem = json.loads(r.blopp('sender')[1])
+        mensagem = json.loads(r.blpop('sender')[1])
         # Simulando envio de e-mail...
         print('Mandando a mensagem:', mensagem['assunto'])
         sleep(randint(15, 45))
